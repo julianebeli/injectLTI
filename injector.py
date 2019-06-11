61d448a7f668327de82272ce6c7e521c37db3cfb
Ebeli <julian.ebeli@education.tas.gov.au>
2019-06-11 12:59:57 +1000

    undid some changes

--- injector.py
+++ injector.py
@@ -3,8 +3,9 @@ import __init__
 import config
 from api.requestor2 import API
 from context_ids import context_id
-contexts = context_id[:5]
-contexts = [31, ]
+contexts = context_id
+# contexts = context_id[:5]
+# contexts = [31, ]
 
 cache_time = 60 * 60 * 24 * 5
 api = API('beta')
@@ -18,7 +19,7 @@ def remover():
         params = dict(methodname=method, account_id=context)
         api.add_method(**params)
         api.do()
-        print(json.dumps(api.results, indent=4))
+        # print(json.dumps(api.results, indent=4))
         if not api.response_error:
             for response in api.results:
                 remove_lti.append(dict(methodname=delete_method, account_id=context,
@@ -40,10 +41,10 @@ def installer():
         methodname='create_external_tool_accounts'
     )
     for context in contexts:
-        LTI_settings.update(dict(course_id=context))
+        LTI_settings.update(dict(account_id=context))
         api.add_method(**LTI_settings)
         api.do()
-        print(json.dumps(api.results, indent=4))
+        # print(json.dumps(api.results, indent=4))
 
 
 if __name__ == '__main__':
